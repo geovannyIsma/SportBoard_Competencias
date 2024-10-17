30d872ff47e7f5dcda9db7089819d170a8ca5bef
edisoncor <edisoncor@gmail.com>
2024-10-17 09:47:11 -0500

    Gitignore

--- frontend/frontend/src/app/app.component.ts
+++ frontend/frontend/src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./core/header/header.component";
+import { FooterComponent } from "./core/footer/footer.component";
+import { SidebarComponent } from "./core/sidebar/sidebar.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet, HeaderComponent, FooterComponent, SidebarComponent],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'frontend';
+    title = 'frontend';
 }
