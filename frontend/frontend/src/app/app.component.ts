ec4e77a7dd2b68ba702bc090dcb54c06f9736730
edisoncor <edisoncor@gmail.com>
2024-10-18 14:51:55 -0500

    Home
    Configuración del Home

--- frontend/frontend/src/app/app.component.ts
+++ frontend/frontend/src/app/app.component.ts
@@ -1,16 +1,29 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./core/header/header.component";
-import { FooterComponent } from "./core/footer/footer.component";
-import { SidebarComponent } from "./core/sidebar/sidebar.component";
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { SidebarComponent } from './core/sidebar/sidebar.component';
+import { SharedModule } from './shared/shared.module';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet, HeaderComponent, FooterComponent, SidebarComponent],
+    imports: [
+        FooterComponent,
+        HeaderComponent,
+        RouterModule,
+        RouterOutlet,
+        SharedModule,
+        SidebarComponent,
+    ],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
 })
 export class AppComponent {
     title = 'frontend';
+    isSidebarOpen = true;
+
+    onToggleSidebar() {
+        this.isSidebarOpen = !this.isSidebarOpen;
+    }
 }
--- frontend/frontend/src/app/core/sidebar/sidebar.component.ts
+++ frontend/frontend/src/app/core/sidebar/sidebar.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { SharedModule } from '../../shared/shared.module';
+import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'app-sidebar',
-  standalone: true,
-  imports: [],
-  templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss'
+    selector: 'app-sidebar',
+    standalone: true,
+    imports: [RouterModule, SharedModule],
+    templateUrl: './sidebar.component.html',
+    styleUrl: './sidebar.component.scss',
 })
 export class SidebarComponent {
+
+    isSidebarOpen = true;
+
+    toggleSidebar(sidenav: any) {
+        sidenav.toggle();
+    }
 
 }
