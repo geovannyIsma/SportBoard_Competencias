0b8fe3fd1a3046bebcc85ee608bf8473a87f9fe6
santigt11 <santiagoguachizaca11@gmail.com>
2024-10-22 22:12:29 -0500

    Cambio Sorteo
    Agregación de mini menú en Calendario y enlazamiento a sorteo

--- frontend/frontend/src/app/calendario/calendario.module.ts
+++ frontend/frontend/src/app/calendario/calendario.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { CalendarioRoutingModule } from './calendario-routing.module';
+import { SorteoComponent } from './sorteo/sorteo.component';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    CalendarioRoutingModule
-  ]
+    CalendarioRoutingModule,
+    SorteoComponent
+  ],
+  exports: [SorteoComponent]
 })
 export class CalendarioModule { }
