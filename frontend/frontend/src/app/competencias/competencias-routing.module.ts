a588e46159f00bba98fc34f6211f4bc8e2a1b272
Abel Mora <abel.mora@unl.edu.ec>
2024-10-22 21:23:24 -0500

    Implemnetación de la interfaz jugador

--- frontend/frontend/src/app/competencias/competencias-routing.module.ts
+++ frontend/frontend/src/app/competencias/competencias-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
-
+import { GestionCompetenciaComponent } from '../competencias/GestionCompetencia/gestion-competencia.component';
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'gestion-competencia',
+    pathMatch: 'full'
+  },
+  {
+    path: 'gestion-competencia',
+    component: GestionCompetenciaComponent
+  }
+];
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
--- frontend/frontend/src/app/competencias/competencias.module.ts
+++ frontend/frontend/src/app/competencias/competencias.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CompetenciasRoutingModule } from './competencias-routing.module';
-
-
+import { GestionCompetenciaComponent } from '../competencias/GestionCompetencia/gestion-competencia.component';
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    CompetenciasRoutingModule
+    CompetenciasRoutingModule,
   ]
 })
+
 export class CompetenciasModule { }
