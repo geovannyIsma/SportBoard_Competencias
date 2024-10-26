6b01c865f8f4ad408e0869c2048854dd6ac5f234
Gerardo Herrera <gerardo.herrera@unl.edu.ec>
2024-10-25 23:01:43 -0500

    Diseño de CalendarMenu

--- frontend/frontend/src/app/core/sidebar/sidebar.component.ts
+++ frontend/frontend/src/app/core/sidebar/sidebar.component.ts
@@ -1,15 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { SharedModule } from '../../shared/shared.module';
 import { RouterModule } from '@angular/router';
 import { MatMenuModule } from '@angular/material/menu';
+import { NgIf } from '@angular/common';
 
 @Component({
     selector: 'app-sidebar',
     standalone: true,
-    imports: [RouterModule, SharedModule, MatMenuModule],
+    imports: [RouterModule, SharedModule, MatMenuModule, NgIf],
     templateUrl: './sidebar.component.html',
     styleUrl: './sidebar.component.scss',
 })
+
+export class SlidebarComponent {
+    @Output() showMenu = new EventEmitter<void>();
+
+    isCalendarMenuOpen = false; // controla la visibilidad del menú
+
+    toggleCalendarMenu() {
+        this.isCalendarMenuOpen = !this.isCalendarMenuOpen; // Alterna entre mostrado y no mostrado cuando se lo llame
+    }
+}
+
 export class SidebarComponent {
 
     isSidebarOpen = true;
