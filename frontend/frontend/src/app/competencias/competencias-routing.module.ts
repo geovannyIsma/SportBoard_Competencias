20dd148a7db9b471a07eab8bc9fa0956f93b0f91
Brian Fernando Aguinsaca L <brian.aguinsaca@unl.edu.ec>
2024-10-27 15:55:50 -0500

    Correcciones Componente Perfil Jugador

--- frontend/frontend/src/app/competencias/competencias-routing.module.ts
+++ frontend/frontend/src/app/competencias/competencias-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+//Importe del componente
+import { PerfilJugadorComponent } from '../competencias/perfil-jugador/perfil-jugador.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'perfil-jugador',  // Redirige automáticamente al componente Perfil Jugador
+    pathMatch: 'full'
+  },
+  {
+    path: 'perfil-jugador',
+    component: PerfilJugadorComponent //Ruta del componente
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- frontend/frontend/src/app/competencias/competencias.module.ts
+++ frontend/frontend/src/app/competencias/competencias.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CompetenciasRoutingModule } from './competencias-routing.module';
+import { PerfilJugadorComponent } from '../competencias/perfil-jugador/perfil-jugador.component';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    CompetenciasRoutingModule
+    CompetenciasRoutingModule,
+    MatIconModule
   ]
 })
 export class CompetenciasModule { }
