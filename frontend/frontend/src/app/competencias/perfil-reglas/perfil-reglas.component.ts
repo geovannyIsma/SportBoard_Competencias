2e95da0bb02a7268333a413fcce6f082eb8bcab9
Abel Mora <abel.mora@unl.edu.ec>
2024-10-26 22:08:36 -0500

    Creación componente Perfil_Reglas

---
+++ frontend/frontend/src/app/competencias/perfil-reglas/perfil-reglas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-perfil-reglas',
+  standalone: true,
+  imports: [],
+  templateUrl: './perfil-reglas.component.html',
+  styleUrl: './perfil-reglas.component.scss'
+})
+export class PerfilReglasComponent {
+
+}
