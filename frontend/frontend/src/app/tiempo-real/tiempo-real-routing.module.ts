931aa40d593a88e5b614b766bb014389ac191335
Steven Luna <133796670+StevenLunaG@users.noreply.github.com>
2024-11-06 20:08:31 -0500

    Update tiempo-real-routing.module.ts

--- frontend/frontend/src/app/tiempo-real/tiempo-real-routing.module.ts
+++ frontend/frontend/src/app/tiempo-real/tiempo-real-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SelecPartidoComponent } from './selec-partido/selec-partido.component';
+import { CronologiaComponent } from './cronologia/cronologia.component';
+import { TrEstadisticaComponent } from './tr-estadistica/tr-estadistica.component';
+import { ArbitroComponent } from './arbitro/arbitro.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: SelecPartidoComponent
+},{
+  path: 'cronologia',
+  component: CronologiaComponent
+},
+{
+  path: 'estadistica',
+  component: TrEstadisticaComponent
+},
+{
+  path: 'arbitro',
+  component: ArbitroComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
