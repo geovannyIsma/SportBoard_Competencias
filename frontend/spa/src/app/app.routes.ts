e8a40eaeead7b13818b70b29c4600f06d2b25319
Edison Coronel <edisoncor@gmail.com>
2025-01-09 08:13:25 -0500

    Agregar componentes de catálogos y grupos, incluyendo estilos y estructura HTML, así como la configuración del módulo de catálogos.

--- frontend/spa/src/app/app.routes.ts
+++ frontend/spa/src/app/app.routes.ts
@@ -3,22 +3,40 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     {
         path: 'usuarios',
-        loadChildren: () => import('./usuario/usuario.module').then(m => m.UsuarioModule)
+        loadChildren: () =>
+            import('./usuario/usuario.module').then((m) => m.UsuarioModule),
     },
     {
         path: 'competencias',
-        loadChildren: () => import('./competencias/competencias.module').then(m => m.CompetenciasModule)
+        loadChildren: () =>
+            import('./competencias/competencias.module').then(
+                (m) => m.CompetenciasModule
+            ),
     },
     {
         path: 'calendario',
-        loadChildren: () => import('./calendario/calendario.module').then(m => m.CalendarioModule)
+        loadChildren: () =>
+            import('./calendario/calendario.module').then(
+                (m) => m.CalendarioModule
+            ),
     },
     {
         path: 'tiempo-real',
-        loadChildren: () => import('./tiempo-real/tiempo-real.module').then(m => m.TiempoRealModule)
+        loadChildren: () =>
+            import('./tiempo-real/tiempo-real.module').then(
+                (m) => m.TiempoRealModule
+            ),
     },
     {
         path: 'estadisticas',
-        loadChildren: () => import('./estadistica/estadistica.module').then(m => m.EstadisticaModule)
+        loadChildren: () =>
+            import('./estadistica/estadistica.module').then(
+                (m) => m.EstadisticaModule
+            ),
     },
-]   ;
+    {
+        path: 'catalogos',
+        loadChildren: () =>
+            import('./catalogs/catalogs.module').then((m) => m.CatalogsModule),
+    },
+];
---
+++ frontend/spa/src/app/catalogs/catalogs/catalogs.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-catalogs',
+    templateUrl: './catalogs.component.html',
+    styleUrl: './catalogs.component.scss',
+})
+export class CatalogsComponent {}
