1245920a0521c853da51859cf611837ada2ac4ab
Edison Coronel <edisoncor@gmail.com>
2025-01-10 21:30:26 -0500

    Actualizar la configuración de CORS para permitir el origen en el puerto 4200 y agregar el proveedor de HttpClient en la configuración de la aplicación.

--- backend/Catalogs/Program.cs
+++ backend/Catalogs/Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddCors(options =>
     options.AddDefaultPolicy(
         builder =>
         {
-            builder.WithOrigins("http://localhost:3000")
+            builder.WithOrigins("http://localhost:4200")
             .AllowAnyHeader()
             .AllowAnyMethod();
         });
