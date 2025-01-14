5c6e607ce812d9784ab7f0a1d79c4e8a0fe77eb3
Geovanny Romero <geovanny.romero@unl.edu.ec>
2025-01-14 08:47:04 -0500

    test api

--- frontend/spa/src/app/competencias/competencias.module.ts
+++ frontend/spa/src/app/competencias/competencias.module.ts
@@ -1,7 +1,6 @@
-
-
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'; // Add this import
 
 import { CompetenciasRoutingModule } from './competencias-routing.module';
 import { PerfilesComponent } from './perfiles/perfiles.component';
@@ -9,25 +8,35 @@ import { PerfilJugadorComponent } from './perfil-jugador/perfil-jugador.componen
 import { GestionCompetenciaComponent } from './GestionCompetencia/gestion-competencia.component';
 import { PerfilEquipoComponent } from './perfil-equipo/perfil-equipo.component';
 import { GestionEquipoComponent } from './gestion-equipo/gestion-equipo.component';
-import {PerfilReglasComponent} from './perfil-reglas/perfil-reglas.component';
+import { PerfilReglasComponent } from './perfil-reglas/perfil-reglas.component';
 import { MatIconModule } from '@angular/material/icon';
+import { ApiService } from './services/api.service';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import { TestApiComponent } from './test-api/test-api.component';
+import { AppComponent } from '../app.component';
 
 @NgModule({
   declarations: [
+    TestApiComponent,
+
   ],
   imports: [
     CommonModule,
+    FormsModule, // Add this line
     PerfilesComponent,
     PerfilEquipoComponent,
-      PerfilReglasComponent,
+    PerfilReglasComponent,
     CompetenciasRoutingModule,
     PerfilJugadorComponent,
     CompetenciasRoutingModule,
     GestionCompetenciaComponent,
     GestionEquipoComponent,
-    MatIconModule
-  ]
+    MatIconModule,
+    HttpClientModule,
+  ],
+  providers: [
+    ApiService
+  ],
 })
-
 export class CompetenciasModule { }
-
