fa51e80c2ffc280e271991fe9f01a6fa23ee352a
Leonardo Peralta <leonardo.peralta@unl.edu.ec>
2025-01-28 16:13:14 -0500

    cambios de api usuarios

--- backend/users/sportProfile/urls.py
+++ backend/users/sportProfile/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
-from .views import UserViewSet
+
+from .views import NacionalityRegisterAPIView, CreateSuperUserAPIView, NacionalityCreateListAPIView, UserListCreateAPIView, UserDetailAPIView, CreateUserAPIView
 
 urlpatterns = [
-    path('users/', UserViewSet.as_view({'get': 'list'}), name='users'),
-    path('users/<int:pk>/', UserViewSet.as_view({'get': 'retrieve'}), name='user'),
+
+    path('users/', UserListCreateAPIView.as_view(), name='user-list-create'), #obtener y crear usuarios.
+    path('users/<int:pk>/', UserDetailAPIView.as_view(), name='user-detail'), #obtener, actualizar y eliminar un usuario específico.
+    path('users/create/', CreateUserAPIView.as_view(), name='create-user'), #crear un usuario.
+
+    path('create-superuser/', CreateSuperUserAPIView.as_view(), name='create-superuser'), #crear un superusuario.
+
+    path('nacionalities/', NacionalityCreateListAPIView.as_view(), name='nacionality-list-create'),
+    path('nacionalities/<int:pk>/', NacionalityRegisterAPIView.as_view(), name='nacionality-register'),
+
 ]
