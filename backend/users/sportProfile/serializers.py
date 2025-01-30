da55d34ff1e60429b73e6ecd3a0ab5638fa8c0bb
Leonardo Peralta <leonardo.peralta@unl.edu.ec>
2025-01-29 21:41:07 -0500

    reparo de servicio docker

--- backend/users/sportProfile/serializers.py
+++ backend/users/sportProfile/serializers.py
@@ -1,8 +1,18 @@
 from rest_framework import serializers
-from .models import User
+from .models import User, Nacionality
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'nationality', 'birthdate', 'weigth']
+        fields = ['id', 'username', 'nationality', 'birthdate', 'weigth', 'role', 'status', 'email', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_superuser(**validated_data)
+        return user
+
+class NacionalitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Nacionality
+        fields = ['id', 'nacionality_name']
